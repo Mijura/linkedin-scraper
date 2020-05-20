f36067212adee0fdb4ea7add30793ecaed0f6437
Miodrag Vilotijevic <miodragvilotijevic@gmail.com>
2020-05-20 12:24:17 +0200

    scraping page by page

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import csv
 import os
+import time
 
 if not os.path.exists('companies'):
     os.makedirs('companies')
@@ -27,28 +28,38 @@ letters = driver.find_elements_by_xpath("//*[@id=\"seo-dir\"]/div/div[2]/div/ol/
 i = 0
 letters_links=[]
 while(i<len(letters)):
-    letters_links.append({"text": letters[i]. text, "href": letters[i].get_attribute('href')})
+    letters_links.append({"text": letters[i].text, "href": letters[i].get_attribute('href')})
     i+=1
 
-print(letters_links)
 for letter in letters_links:
-    print(letter)
+
     driver.get(letter["href"])
     l = letter["text"]
-
-    elem = driver.find_elements_by_xpath("//*[@id=\"seo-dir\"]/div/div[3]/div/ul/li/*")
 
     if not os.path.exists('companies/'+str(l)):
         os.makedirs('companies/'+str(l))
 
-    with open('companies/'+str(l)+'/'+str(l)+'.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Company name", "Linkedin"])
+    pages = driver.find_elements_by_xpath("//*[@id=\"seo-dir\"]/div/div[4]/div/ol/li/*")
+    
+    i = 0
+    pages_links=[]
+    while(i<len(pages)):
+        pages_links.append({"text": pages[i].text, "href": pages[i].get_attribute('href')})
+        i+=1
 
-        i=1
-        for company_link in elem:
-            writer.writerow([i, company_link.text, company_link.get_attribute('href')])
-            i+=1
+    for page in pages_links:
+
+        driver.get(page["href"])
+
+        elem = driver.find_elements_by_xpath("//*[@id=\"seo-dir\"]/div/div[3]/ul/li/*")
+        with open('companies/'+str(l)+'/'+page["text"]+'.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Company name", "Linkedin"])
+
+            i=1
+            for company_link in elem:
+                writer.writerow([i, company_link.text, company_link.get_attribute('href')])
+                i+=1
 
     
 
